826a7e2dc1de2a2603a8b481ed381d134cf8f4a1
Cindy Perat <cindy.perat@gmail.com>
2019-02-15 10:30:08 +0100

    refactor: adjust layout for iPhone SE

--- Instagrid/View/LayoutView.swift
+++ Instagrid/View/LayoutView.swift
@@ -10,28 +10,9 @@ import UIKit
 
 class LayoutView: UIView {
     @IBOutlet private var image1Container: UIView!
-    @IBOutlet private var image1ContainerWidth: NSLayoutConstraint!
-    @IBOutlet private var image1ContainerHeight: NSLayoutConstraint!
-    @IBOutlet private var image1ContainerLeadingSpace: NSLayoutConstraint!
-    @IBOutlet private var image1ContainerTopSpace: NSLayoutConstraint!
-    
     @IBOutlet private var image2Container: UIView!
-    @IBOutlet private var image2ContainerWidth: NSLayoutConstraint!
-    @IBOutlet private var image2ContainerHeight: NSLayoutConstraint!
-    @IBOutlet private var image2ContainerLeadingSpace: NSLayoutConstraint!
-    @IBOutlet private var image2ContainerTopSpace: NSLayoutConstraint!
-    
     @IBOutlet private var image3Container: UIView!
-    @IBOutlet private var image3ContainerWidth: NSLayoutConstraint!
-    @IBOutlet private var image3ContainerHeight: NSLayoutConstraint!
-    @IBOutlet private var image3ContainerLeadingSpace: NSLayoutConstraint!
-    @IBOutlet private var image3ContainerTopSpace: NSLayoutConstraint!
-    
     @IBOutlet private var image4Container: UIView!
-    @IBOutlet private var image4ContainerWidth: NSLayoutConstraint!
-    @IBOutlet private var image4ContainerHeight: NSLayoutConstraint!
-    @IBOutlet private var image4ContainerLeadingSpace: NSLayoutConstraint!
-    @IBOutlet private var image4ContainerTopSpace: NSLayoutConstraint!
     
     @IBOutlet private var layoutSelection: UIView!
         
@@ -48,64 +29,24 @@ class LayoutView: UIView {
     private func setLayout(_ layout: Layout) {
         switch layout {
         case .layout1:
-            image1ContainerWidth.constant = 270
-            image1ContainerHeight.constant = 127.5
-            image1ContainerLeadingSpace.constant = 15
-            image1ContainerTopSpace.constant = 15
-            
-            image2ContainerWidth.constant = 127.5
-            image2ContainerHeight.constant = 127.5
-            image2ContainerLeadingSpace.constant = 15
-            image2ContainerTopSpace.constant = 157.5
-            
-            image3ContainerWidth.constant = 127.5
-            image3ContainerHeight.constant = 127.5
-            image3ContainerLeadingSpace.constant = 157.7
-            image3ContainerTopSpace.constant = 157.5
-            
-            image4Container.isHidden = true
+            image1Container.isHidden = false
+            image2Container.isHidden = true
+            image3Container.isHidden = false
+            image4Container.isHidden = false
             
             layoutSelection.frame.origin.x = 0
         case .layout2:
-            image1ContainerWidth.constant = 127.5
-            image1ContainerHeight.constant = 127.5
-            image1ContainerLeadingSpace.constant = 15
-            image1ContainerTopSpace.constant = 15
-            
-            image2ContainerWidth.constant = 127.5
-            image2ContainerHeight.constant = 127.5
-            image2ContainerLeadingSpace.constant = 157.5
-            image2ContainerTopSpace.constant = 15
-            
-            image3ContainerWidth.constant = 270
-            image3ContainerHeight.constant = 127.5
-            image3ContainerLeadingSpace.constant = 15
-            image3ContainerTopSpace.constant = 157.5
-            
+            image1Container.isHidden = false
+            image2Container.isHidden = false
+            image3Container.isHidden = false
             image4Container.isHidden = true
             
             layoutSelection.frame.origin.x = 100
         case .layout3:
-            image1ContainerWidth.constant = 127.5
-            image1ContainerHeight.constant = 127.5
-            image1ContainerLeadingSpace.constant = 15
-            image1ContainerTopSpace.constant = 15
-            
-            image2ContainerWidth.constant = 127.5
-            image2ContainerHeight.constant = 127.5
-            image2ContainerLeadingSpace.constant = 157.5
-            image2ContainerTopSpace.constant = 15
-            
-            image3ContainerWidth.constant = 127.5
-            image3ContainerHeight.constant = 127.5
-            image3ContainerLeadingSpace.constant = 15
-            image3ContainerTopSpace.constant = 157.5
-            
+            image1Container.isHidden = false
+            image2Container.isHidden = false
+            image3Container.isHidden = false
             image4Container.isHidden = false
-            image4ContainerWidth.constant = 127.5
-            image4ContainerHeight.constant = 127.5
-            image4ContainerLeadingSpace.constant = 157.5
-            image4ContainerTopSpace.constant = 157.5
             
             layoutSelection.frame.origin.x = 200
         }
